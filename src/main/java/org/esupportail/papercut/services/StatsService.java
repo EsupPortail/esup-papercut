8dc9569503e687734df6fc580be87603534df437
Vincent Bonamy <vbonamy@gmail.com>
2016-07-08 10:07:02 +0200

    Chart.js -> 2.1.6

--- src/main/java/org/esupportail/papercut/services/StatsService.java
+++ src/main/java/org/esupportail/papercut/services/StatsService.java
@@ -35,17 +35,19 @@ public class StatsService {
     	
    		for (Object result : listes) {
    			Object[] r = (Object[]) result;	
+   			String annee = String.valueOf(Math.round(Double.parseDouble(r[0].toString())));
+   			String mois = String.valueOf(Math.round(Double.parseDouble(r[1].toString())));
    			if (level == 2){
-	   		    map.put(r[0].toString(),r[1]);
+	   		    map.put(annee,r[1]);
    			}else{
-   				if(test== null || test.equals(r[0].toString())){
-   					secondMap.put(r[1].toString(),r[2]);
+   				if(test== null || test.equals(annee)){
+   					secondMap.put(mois,r[2]);
    				}else{
    					map.put(test, secondMap);
    					secondMap = new LinkedHashMap<String, Object>();
-   					secondMap.put(r[1].toString(),r[2]);
+   					secondMap.put(mois,r[2]);
    				}
-   				test = r[0].toString();
+   				test = annee;
    				if(i ==  listes.size()){
    					map.put(test, secondMap);
    				}
--- src/main/webapp/js/esupPapercut.js
+++ src/main/webapp/js/esupPapercut.js
@@ -18,29 +18,26 @@ $(document).ready(function() {
 	        	var coefMontant = 100;
 	        	var color = ["151,187,205,0.2", "220,220,220,0.2", "35, 60, 72, 0.2", "138, 235, 254, 0.2"]
 	        	var fullColor = ["151,187,205,1", "220,220,220,1", "35, 60, 72, 1", "138, 235, 254, 1"]
-	        	var legends = [];
 	        	var a=0;
-	        	
 	    		$.each(data.nombre, function(index, value) {
-	    			//legendes
-	    			legends[index]="rgba("+fullColor[a]+")";
 	    			var keyMois = Object.keys(value);
+	    			console.log(value);
 	    			for(i=1;i<=12;i++){
 	        			inlineLabels.push(i);
-	        			if($.inArray(i.toFixed(1), keyMois)>-1){
-			        		inlineValeurs.push(value[i.toFixed(1)]);
+	        			if($.inArray(i.toString(), keyMois)>-1){
+			        		inlineValeurs.push(value[i]);
 	        			}else{
 			        		inlineValeurs.push(null);
 	        			}			
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-	 		            fillColor: "rgba("+color[a]+")",
-	 		            strokeColor: "rgba("+fullColor[a]+")",
-	 		            pointColor: "rgba("+fullColor[a]+")",
-	 		            pointStrokeColor: "#fff",
-	 		            pointHighlightFill: "#fff",
-	 		            pointHighlightStroke: "rgba("+fullColor[a]+")",
+     		            backgroundColor: "rgba("+color[a]+")",
+     		            borderColor: "rgba("+fullColor[a]+")",
+     		            pointColor: "rgba("+fullColor[a]+")",
+     		            pointBorderColor: "#fff",
+     		            pointHoverBorderColor: "#fff",
+     		            pointBackgroundColor: "rgba("+color[a]+")",
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
@@ -49,17 +46,29 @@ $(document).ready(function() {
 	     		    datasets: inlineDatasets
 	     		};       	
 	         	var ctx3 = $("#transactionsNombreChart").get(0).getContext("2d");
-	         	var myLineChart = new Chart(ctx3).Line(dataMois,{
-	         		//responsive : true
-	         	});  
-	         	//Légende
-	         	var years = "<br />";
-	         	for (var key in legends){
-	         		var splitKey = key.split("."); 
-	         		var year = "<span style='padding:3px;margin-left:2px;color:white;font-weight:bold;background-color:" + legends[key] +";'>"+ splitKey[0] + "</span>";
-	         		years += year;
-	         	}
-	         	$("#transactionsNombreChart").after(years);
+	         	var myLineChart = new Chart(ctx3, {
+	        		type: 'line',
+	        		data: dataMois,
+	        		options: {
+	        			responsive: true,
+	        			 scales:{
+	        	                xAxes:[{
+	        	        			ticks: {
+	        	        				autoSkip: false
+	        	        			}
+	        	                }]
+	        	            },
+			                legend: {
+			                    position: 'bottom'
+			                },
+			                tooltips: {
+			                	mode: 'label'
+			                },
+			                hover: {
+			                    mode: 'dataset'
+			                }
+	        		}
+	        	});  	
 	         	//Montants       	
 	         	var inlineTitles = [];
 	         	var inlineLabels = [];
@@ -67,28 +76,25 @@ $(document).ready(function() {
 	        	var inlineDatasets = [];
 	        	var color = ["151,187,205,0.2", "220,220,220,0.2", "35, 60, 72, 0.2", "138, 235, 254, 0.2"]
 	        	var fullColor = ["151,187,205,1", "220,220,220,1", "35, 60, 72, 1", "138, 235, 254, 1"]
-	        	var legends = [];
 	        	var a=0;
 	    		$.each(data.montants, function(index, value) {
-	    			//legendes
-	    			legends[index]="rgba("+fullColor[a]+")";
 	    			var keyMois = Object.keys(value);
 	    			for(i=1;i<=12;i++){
 	        			inlineLabels.push(i);
-	        			if($.inArray(i.toFixed(1), keyMois)>-1){
-			        		inlineValeurs.push(value[i.toFixed(1)]/coefMontant);
+	        			if($.inArray(i.toString(), keyMois)>-1){
+			        		inlineValeurs.push(value[i]/coefMontant);
 	        			}else{
 			        		inlineValeurs.push(null);
 	        			}			
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-	 		            fillColor: "rgba("+color[a]+")",
-	 		            strokeColor: "rgba("+fullColor[a]+")",
-	 		            pointColor: "rgba("+fullColor[a]+")",
-	 		            pointStrokeColor: "#fff",
-	 		            pointHighlightFill: "#fff",
-	 		            pointHighlightStroke: "rgba("+fullColor[a]+")",
+     		            backgroundColor: "rgba("+color[a]+")",
+     		            borderColor: "rgba("+fullColor[a]+")",
+     		            pointColor: "rgba("+fullColor[a]+")",
+     		            pointBorderColor: "#fff",
+     		            pointHoverBorderColor: "#fff",
+     		            pointBackgroundColor: "rgba("+color[a]+")",
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
@@ -97,17 +103,32 @@ $(document).ready(function() {
 	     		    datasets: inlineDatasets
 	     		};       	
 	         	var ctx3 = $("#transactionsMontantChart").get(0).getContext("2d");
-	         	var myLineChart = new Chart(ctx3).Line(dataMois,{
-	         		//responsive : true
-	         	});  
-	         	//Légende
-	         	var years = "<br />";
-	         	for (var key in legends){
-	         		var splitKey = key.split("."); 
-	         		var year = "<span style='padding:3px;margin-left:2px;color:white;font-weight:bold;background-color:" + legends[key] +";'>"+ splitKey[0] + "</span>";
-	         		years += year;
-	         	}
-	         	$("#transactionsMontantChart").after(years);  	
+	         	var myLineChart = new Chart(ctx3, {
+	        		type: 'line',
+	        		data: dataMois,
+	        		options: {
+	        			 scales:{
+	        				 type:"category",
+	        	                xAxes:[{
+	        	        			ticks: {
+	        	        				autoSkip: false
+	        	        			}
+	        	                }],
+	        	                yAxes:[{
+	        	                	type: "linear"
+	        	                }]
+	        	            },
+			                legend: {
+			                    position: 'bottom'
+			                },
+			                tooltips: {
+			                	mode: 'label'
+			                },
+			                hover: {
+			                    mode: 'dataset'
+			                }
+	        		}
+	        	});  
 	         	//Cumul Nb de transactions       	
 	         	var inlineTitles = [];
 	         	var inlineLabels = [];
@@ -115,28 +136,25 @@ $(document).ready(function() {
 	        	var inlineDatasets = [];
 	        	var color = ["151,187,205,0.2", "220,220,220,0.2", "35, 60, 72, 0.2", "138, 235, 254, 0.2"]
 	        	var fullColor = ["151,187,205,1", "220,220,220,1", "35, 60, 72, 1", "138, 235, 254, 1"]
-	        	var legends = [];
 	        	var a=0;
 	    		$.each(data.cumulTransac, function(index, value) {
-	    			//legendes
-	    			legends[index]="rgba("+fullColor[a]+")";
 	    			var keyMois = Object.keys(value);
 	    			for(i=1;i<=12;i++){
 	        			inlineLabels.push(i);
-	        			if($.inArray(i.toFixed(1), keyMois)>-1){
-			        		inlineValeurs.push(value[i.toFixed(1)]);
+	        			if($.inArray(i.toString(), keyMois)>-1){
+			        		inlineValeurs.push(value[i]);
 	        			}else{
 			        		inlineValeurs.push(null);
 	        			}			
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-	 		            fillColor: "rgba("+color[a]+")",
-	 		            strokeColor: "rgba("+fullColor[a]+")",
-	 		            pointColor: "rgba("+fullColor[a]+")",
-	 		            pointStrokeColor: "#fff",
-	 		            pointHighlightFill: "#fff",
-	 		            pointHighlightStroke: "rgba("+fullColor[a]+")",
+     		            backgroundColor: "rgba("+color[a]+")",
+     		            borderColor: "rgba("+fullColor[a]+")",
+     		            pointColor: "rgba("+fullColor[a]+")",
+     		            pointBorderColor: "#fff",
+     		            pointHoverBorderColor: "#fff",
+     		            pointBackgroundColor: "rgba("+color[a]+")",
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
@@ -145,17 +163,31 @@ $(document).ready(function() {
 	     		    datasets: inlineDatasets
 	     		};       	
 	         	var ctx3 = $("#transactionsCumulTransactionsChart").get(0).getContext("2d");
-	         	var myLineChart = new Chart(ctx3).Line(dataMois,{
-	         		//responsive : true
-	         	});  
-	         	//Légende
-	         	var years = "<br />";
-	         	for (var key in legends){
-	         		var splitKey = key.split("."); 
-	         		var year = "<span style='padding:3px;margin-left:2px;color:white;font-weight:bold;background-color:" + legends[key] +";'>"+ splitKey[0] + "</span>";
-	         		years += year;
-	         	}
-	         	$("#transactionsCumulTransactionsChart").after(years);  	
+	         	var myLineChart = new Chart(ctx3, {
+	        		type: 'line',
+	        		data: dataMois,
+	        		options: {
+	        			 responsive: true,
+	        			 scales:{
+	        				 type:"category",
+	        	                xAxes:[{
+	        	        			ticks: {
+	        	        				autoSkip: false
+	        	        			}
+	        	                }]
+	        	            },
+			                legend: {
+			                    position: 'bottom'
+			                },
+			                tooltips: {
+			                	mode: 'label'
+			                },
+			                hover: {
+			                    mode: 'dataset'
+			                }	                
+	        	            
+	        		}
+	        	});   	
 	         	//Cumul Montant des transactions
 	         	var inlineTitles = [];
 	         	var inlineLabels = [];
@@ -163,28 +195,25 @@ $(document).ready(function() {
 	        	var inlineDatasets = [];
 	        	var color = ["151,187,205,0.2", "220,220,220,0.2", "35, 60, 72, 0.2", "138, 235, 254, 0.2"]
 	        	var fullColor = ["151,187,205,1", "220,220,220,1", "35, 60, 72, 1", "138, 235, 254, 1"]
-	        	var legends = [];
 	        	var a=0;
 	    		$.each(data.cumulMontant, function(index, value) {
-	    			//legendes
-	    			legends[index]="rgba("+fullColor[a]+")";
 	    			var keyMois = Object.keys(value);
 	    			for(i=1;i<=12;i++){
 	        			inlineLabels.push(i);
-	        			if($.inArray(i.toFixed(1), keyMois)>-1){
-			        		inlineValeurs.push(value[i.toFixed(1)]/coefMontant);
+	        			if($.inArray(i.toString(), keyMois)>-1){
+			        		inlineValeurs.push(value[i]/coefMontant);
 	        			}else{
 			        		inlineValeurs.push(null);
 	        			}			
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-	 		            fillColor: "rgba("+color[a]+")",
-	 		            strokeColor: "rgba("+fullColor[a]+")",
-	 		            pointColor: "rgba("+fullColor[a]+")",
-	 		            pointStrokeColor: "#fff",
-	 		            pointHighlightFill: "#fff",
-	 		            pointHighlightStroke: "rgba("+fullColor[a]+")",
+     		            backgroundColor: "rgba("+color[a]+")",
+     		            borderColor: "rgba("+fullColor[a]+")",
+     		            pointColor: "rgba("+fullColor[a]+")",
+     		            pointBorderColor: "#fff",
+     		            pointHoverBorderColor: "#fff",
+     		            pointBackgroundColor: "rgba("+color[a]+")",
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
@@ -193,17 +222,29 @@ $(document).ready(function() {
 	     		    datasets: inlineDatasets
 	     		};       	
 	         	var ctx3 = $("#transactionsCumulMontantChart").get(0).getContext("2d");
-	         	var myLineChart = new Chart(ctx3).Line(dataMois,{
-	         		//responsive : true
-	         	});  
-	         	//Légende
-	         	var years = "<br />";
-	         	for (var key in legends){
-	         		var splitKey = key.split("."); 
-	         		var year = "<span style='padding:3px;margin-left:2px;color:white;font-weight:bold;background-color:" + legends[key] +";'>"+ splitKey[0] + "</span>";
-	         		years += year;
-	         	}
-	         	$("#transactionsCumulMontantChart").after(years);  
+	         	var myLineChart = new Chart(ctx3, {
+	        		type: 'line',
+	        		data: dataMois,
+	        		options: {
+	        			 responsive: true,
+	        			 scales:{
+	        	                xAxes:[{
+	        	        			ticks: {
+	        	        				autoSkip: false
+	        	        			}
+	        	                }]
+	        	            },
+			                legend: {
+			                    position: 'bottom'
+			                },
+			                tooltips: {
+			                	mode: 'label'
+			                },
+			                hover: {
+			                    mode: 'dataset'
+			                }
+	        		}
+	        	});   	 
 	        }
 	    });
 	}
