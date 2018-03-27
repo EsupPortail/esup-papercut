fb7ed6ed36c82ca71531bd7f0bd66600b2ac556f
Vincent Bonamy <vbonamy@gmail.com>
2018-03-27 13:28:07 +0200

    Maj Chart JS and fix -> Thanks JP Tran :)

--- src/main/webapp/js/esupPapercut.js
+++ src/main/webapp/js/esupPapercut.js
@@ -1,3 +1,18 @@
+//Couleurs pour graphiques stats
+var seed = 11;
+function random() {
+    var x = Math.sin(seed++) * 1000;
+    return x - Math.floor(x);
+}
+var generateColors = [];
+var generateBorderColors = [];
+var generateStackColors = [];
+for(var k = 0; k < 100; k++) {
+	var color = Math.floor(random()*256) + ',' + Math.floor(random()*256) + ',' + Math.floor(random()*256);
+	generateColors.push('rgba(' + color + ', 0.6)');
+	generateStackColors.push('rgba(' + color + ', 0.6)');
+	generateBorderColors.push('rgba(' + color + ', 1)'); 
+}
 
 $(document).ready(function() {
 //Stats 
@@ -16,8 +31,6 @@ $(document).ready(function() {
 	        	var inlineValeurs = [];
 	        	var inlineDatasets = [];
 	        	var coefMontant = 100;
-	        	var color = ["151,187,205,0.2", "220,220,220,0.2", "35, 60, 72, 0.2", "138, 235, 254, 0.2"]
-	        	var fullColor = ["151,187,205,1", "220,220,220,1", "35, 60, 72, 1", "138, 235, 254, 1"]
 	        	var a=0;
 	    		$.each(data.nombre, function(index, value) {
 	    			var keyMois = Object.keys(value);
@@ -32,12 +45,12 @@ $(document).ready(function() {
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-     		            backgroundColor: "rgba("+color[a]+")",
-     		            borderColor: "rgba("+fullColor[a]+")",
-     		            pointColor: "rgba("+fullColor[a]+")",
-     		            pointBorderColor: "#fff",
-     		            pointHoverBorderColor: "#fff",
-     		            pointBackgroundColor: "rgba("+color[a]+")",
+	 			         backgroundColor: generateColors[a],
+	 			         borderColor: generateColors[a],
+	 			         pointColor: generateBorderColors[a],
+	 			         pointBorderColor: "#fff",
+	 			         pointHoverBorderColor: "#fff",
+	 			         pointBackgroundColor: generateColors[a],
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
@@ -89,12 +102,12 @@ $(document).ready(function() {
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-     		            backgroundColor: "rgba("+color[a]+")",
-     		            borderColor: "rgba("+fullColor[a]+")",
-     		            pointColor: "rgba("+fullColor[a]+")",
-     		            pointBorderColor: "#fff",
-     		            pointHoverBorderColor: "#fff",
-     		            pointBackgroundColor: "rgba("+color[a]+")",
+	 			         backgroundColor: generateColors[a],
+	 			         borderColor: generateColors[a],
+	 			         pointColor: generateBorderColors[a],
+	 			         pointBorderColor: "#fff",
+	 			         pointHoverBorderColor: "#fff",
+	 			         pointBackgroundColor: generateColors[a],
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
@@ -149,12 +162,12 @@ $(document).ready(function() {
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-     		            backgroundColor: "rgba("+color[a]+")",
-     		            borderColor: "rgba("+fullColor[a]+")",
-     		            pointColor: "rgba("+fullColor[a]+")",
-     		            pointBorderColor: "#fff",
-     		            pointHoverBorderColor: "#fff",
-     		            pointBackgroundColor: "rgba("+color[a]+")",
+	 			         backgroundColor: generateColors[a],
+	 			         borderColor: generateColors[a],
+	 			         pointColor: generateBorderColors[a],
+	 			         pointBorderColor: "#fff",
+	 			         pointHoverBorderColor: "#fff",
+	 			         pointBackgroundColor: generateColors[a],
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
@@ -208,12 +221,12 @@ $(document).ready(function() {
 	    			}
 	                inlineDatasets.push({
 	 		            label: index,
-     		            backgroundColor: "rgba("+color[a]+")",
-     		            borderColor: "rgba("+fullColor[a]+")",
-     		            pointColor: "rgba("+fullColor[a]+")",
-     		            pointBorderColor: "#fff",
-     		            pointHoverBorderColor: "#fff",
-     		            pointBackgroundColor: "rgba("+color[a]+")",
+	 			         backgroundColor: generateColors[a],
+	 			         borderColor: generateColors[a],
+	 			         pointColor: generateBorderColors[a],
+	 			         pointBorderColor: "#fff",
+	 			         pointHoverBorderColor: "#fff",
+	 			         pointBackgroundColor: generateColors[a],
 	 		            data: inlineValeurs
 	                });a++;inlineValeurs = [];
 	            });	     
